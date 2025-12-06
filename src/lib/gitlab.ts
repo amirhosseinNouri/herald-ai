d6b70e6d40b8181c0b00a9bb33803147c8e51ed8
Amirhossein Nouri <amirhossein.nouri@snapp.cab>
2025-12-06 21:06:52 +0330

    unify error handling

--- src/lib/gitlab.ts
+++ src/lib/gitlab.ts
@@ -6,87 +6,70 @@ import type {
 } from '@/types/gitlab';
 
 const fetchVersionCommits = async (version: string) => {
-  try {
-    const tags = await fetch(
-      `${process.env.HERALD_GITLAB_BASE_URL}/projects/${process.env.HERALD_GITLAB_PROJECT_ID}/repository/tags`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.HERALD_GITLAB_TOKEN}`,
-        },
-      },
-    );
-    const tagsData = (await tags.json()) as GitlabTag[];
-    const semanticTags = tagsData.filter((tag: any) =>
-      tag.name.match(/^v\d+\.\d+\.\d+$/),
-    );
-
-    const tagIndex = semanticTags.findIndex(
-      (tag: GitlabTag) => tag.name === version,
-    );
-    const previousTag = semanticTags[tagIndex + 1];
-
-    if (!previousTag) {
-      throw new Error('Previous tag not found');
-    }
-
-    // Fetch all commits between the version and the previous version
-    const commits = await fetch(
-      `${process.env.HERALD_GITLAB_BASE_URL}/projects/${process.env.HERALD_GITLAB_PROJECT_ID}/repository/compare?from=${previousTag.name}&to=${version}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.HERALD_GITLAB_TOKEN}`,
-        },
-      },
-    );
-
-    const data = (await commits.json()) as { commits: Commit[] };
-
-    const commitsWithoutTags = data.commits.filter(
-      (commit: Commit) => !commit.title.match(/^\d+\.\d+\.\d+(-\S+)?$/),
-    );
-
-    return commitsWithoutTags;
-  } catch (error) {
-    console.error('❌ Failed to fetch version commits:', error);
-    process.exit(1);
-  }
-};
-
-const extractReleaseManager = async () => {
-  try {
-    const response = await fetch(`${process.env.HERALD_GITLAB_BASE_URL}/user`, {
+  const tags = await fetch(
+    `${process.env.HERALD_GITLAB_BASE_URL}/projects/${process.env.HERALD_GITLAB_PROJECT_ID}/repository/tags`,
+    {
       headers: {
         Authorization: `Bearer ${process.env.HERALD_GITLAB_TOKEN}`,
       },
-    });
+    },
+  );
+  const tagsData = (await tags.json()) as GitlabTag[];
+  const semanticTags = tagsData.filter((tag: any) =>
+    tag.name.match(/^v\d+\.\d+\.\d+$/),
+  );
 
-    const user = (await response.json()) as GitlabUer;
-    return user.name;
-  } catch (error) {
-    console.error('❌ Failed to extract release manager:', error);
+  const tagIndex = semanticTags.findIndex(
+    (tag: GitlabTag) => tag.name === version,
+  );
+  const previousTag = semanticTags[tagIndex + 1];
+
+  if (!previousTag) {
+    throw new Error('Previous tag not found');
   }
 
-  return 'unknown';
+  // Fetch all commits between the version and the previous version
+  const commits = await fetch(
+    `${process.env.HERALD_GITLAB_BASE_URL}/projects/${process.env.HERALD_GITLAB_PROJECT_ID}/repository/compare?from=${previousTag.name}&to=${version}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.HERALD_GITLAB_TOKEN}`,
+      },
+    },
+  );
+
+  const data = (await commits.json()) as { commits: Commit[] };
+
+  const commitsWithoutTags = data.commits.filter(
+    (commit: Commit) => !commit.title.match(/^\d+\.\d+\.\d+(-\S+)?$/),
+  );
+
+  return commitsWithoutTags;
+};
+
+const extractReleaseManager = async () => {
+  const response = await fetch(`${process.env.HERALD_GITLAB_BASE_URL}/user`, {
+    headers: {
+      Authorization: `Bearer ${process.env.HERALD_GITLAB_TOKEN}`,
+    },
+  });
+
+  const user = (await response.json()) as GitlabUer;
+  return user.name;
 };
 
 const getProjectDetails = async () => {
-  try {
-    const response = await fetch(
-      `${process.env.HERALD_GITLAB_BASE_URL}/projects/${process.env.HERALD_GITLAB_PROJECT_ID}`,
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.HERALD_GITLAB_TOKEN}`,
-        },
+  const response = await fetch(
+    `${process.env.HERALD_GITLAB_BASE_URL}/projects/${process.env.HERALD_GITLAB_PROJECT_ID}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.HERALD_GITLAB_TOKEN}`,
       },
-    );
+    },
+  );
 
-    const project = (await response.json()) as GitlabProject;
-    return project;
-  } catch (error) {
-    console.error('❌ Failed to get project details:', error);
-  }
-
-  return { name: 'unknown' };
+  const project = (await response.json()) as GitlabProject;
+  return project;
 };
 
 export { fetchVersionCommits, extractReleaseManager, getProjectDetails };
