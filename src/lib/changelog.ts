919318118823670a4f486f107fd51314f56500cb
Amirhossein Nouri <amirhossein.nouri@snapp.cab>
2025-12-08 16:53:23 +0330

    fix: config loading issue

--- src/lib/changelog.ts
+++ src/lib/changelog.ts
@@ -3,23 +3,15 @@ import { AI_SYSTEM_PROMPT } from '@/constants/ai';
 import { logger } from './logger';
 import { OpenAI } from 'openai';
 
-if (!process.env.AI_API_KEY) {
-  throw new Error('AI_API_KEY not provided');
-}
-
-if (!process.env.AI_BASE_URL) {
-  throw new Error('AI_BASE_URL not provided');
-}
-
-const openai = new OpenAI({
-  apiKey: process.env.AI_API_KEY,
-  baseURL: process.env.AI_BASE_URL,
-});
-
 const generateChangelog = async (commits: Commit[]) => {
   if (!process.env.AI_MODEL) {
     throw new Error('AI_MODEL not provided');
   }
+
+  const openai = new OpenAI({
+    apiKey: process.env.AI_API_KEY,
+    baseURL: process.env.AI_BASE_URL,
+  });
 
   const response = await openai.chat.completions.create({
     model: process.env.AI_MODEL,
