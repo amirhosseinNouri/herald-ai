d9a28d505eb6205c20299d03384bca469b9c2c04
Amirhossein Nouri <amirhossein.nouri@snapp.cab>
2025-12-08 16:34:00 +0330

    feat: switch to Liara open AI model

--- src/lib/changelog.ts
+++ src/lib/changelog.ts
@@ -1,25 +1,41 @@
-import { generateText } from 'ai';
-import { ollama } from 'ollama-ai-provider-v2';
 import type { Commit } from '@/types/gitlab';
 import { AI_SYSTEM_PROMPT } from '@/constants/ai';
 import { logger } from './logger';
+import { OpenAI } from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.HERALD_AI_API_KEY,
+  baseURL: process.env.HERALD_AI_BASE_URL,
+});
 
 const generateChangelog = async (commits: Commit[]) => {
   if (!process.env.HERALD_AI_MODEL) {
-    throw new Error('AI_MODEL not provided');
+    throw new Error('HERALD_AI_MODEL not provided');
   }
 
-  const data = await generateText({
-    model: ollama(process.env.HERALD_AI_MODEL),
-    prompt: `Create a changelog for the following commits: ${commits.map(
-      (commit: Commit) => commit.message,
-    )}`,
-    system: AI_SYSTEM_PROMPT,
+  const response = await openai.chat.completions.create({
+    model: process.env.HERALD_AI_MODEL,
+    messages: [
+      {
+        role: 'user',
+        content: `Create a changelog for the following commits: ${commits.map(
+          (commit: Commit) => commit.message,
+        )}`,
+      },
+      {
+        role: 'system',
+        content: AI_SYSTEM_PROMPT,
+      },
+    ],
   });
 
   logger.info('✅ Changelog generated successfully');
 
-  return data.text;
+  if (!response.choices[0]?.message.content) {
+    throw new Error('Failed to generate changelog');
+  }
+
+  return response.choices[0].message.content;
 };
 
 export { generateChangelog };
