12ea280ff038ac0274b2220be031d7aef3f8d56f
Amirhossein Nouri <amirhossein.nouri@snapp.cab>
2025-12-08 21:29:20 +0330

    fix: remove explicit .env

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({
-  path: path.join(process.cwd(), '.env'),
-  debug: process.env.NODE_ENV === 'development',
-});
+import 'dotenv';
 import { intro, spinner } from '@clack/prompts';
 import color from 'picocolors';
 
