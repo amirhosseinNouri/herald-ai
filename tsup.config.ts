d93157ea310dc0928805f1138c5978423dad3cd7
Amirhossein Nouri <amir.h.nouri2000@gmail.com>
2025-12-09 18:52:01 +0330

    fix: bin issue

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs'],
+  outDir: 'dist',
+  clean: true,
+  minify: false,
+  sourcemap: false,
+  target: 'node18',
+  banner: {
+    js: '#!/usr/bin/env node',
+  },
+});
